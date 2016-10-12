59ca2b8009a6eb6c108ccc93722d847e06111f0e
Wassim Metallaoui <wassim@metallaoui.com>
2016-10-11 22:06:33 -0500

    List resources at 36°N

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,4 +1,6 @@
 class ResourcesController < ApplicationController
   def index
+    client = CobotClient::ApiClient.new(current_user.api_token)
+    @resources = client.get_resources('36n')
   end
 end
