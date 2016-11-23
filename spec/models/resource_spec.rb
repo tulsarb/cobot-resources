2927a64f3594d73dad993407e74bb6284dea7aa2
Wassim Metallaoui <wassim@metallaoui.com>
2016-11-23 16:40:16 -0600

    Add model specs for Resource

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Resource, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Resource do
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:integration_id) }
+  it { should validate_presence_of(:integration_raw) }
 end
